18333741437128e4e15566cb0758599ed92517e1
manishr7 <143963938+manishr7@users.noreply.github.com>
2023-12-27 18:23:53 +0530

    final

--- server/index.js
+++ server/index.js
@@ -1,38 +1,40 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-import express from "express"
-import cors from "cors"
-import router from "./Routes/userRoutes.js"
+import express from "express";
+import cors from "cors";
+import router from "./Routes/userRoutes.js";
 
-
-const app=express();
+const app = express();
 const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
+  origin: "*",
+
+  methods: ["GET", "POST"],
+
+  allowedHeaders: ["Content-Type"],
+};
 app.use(cors(corsOpts));
-const port=process.env.PORT;
+const port = process.env.PORT;
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Deployed!");
+});
+app.use("/api/user", router);
 
-app.use("/api/user",router)
-app.listen(port,()=>{
+const DB_URL = process.env.CONNECTION_URL || 8080;
 
-    console.log(`Server running at http://localhost:${port}`)
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(DB_URL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+connectDB().then(() => {
+  app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+  })
 })
-const DB_URL=process.env.CONNECTION_URL;
-mongoose.connect(DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>
-{
-    console.log("MongoDB database connected")
-}).catch((error)=>
-{
-    console.log(error)
-    })
